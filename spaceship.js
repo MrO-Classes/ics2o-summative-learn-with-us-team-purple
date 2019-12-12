1547aeff2930f29ab8c7b56f79922e95f27c9384
RachaelED <55112454+RachaelED@users.noreply.github.com>
2019-12-12 11:55:22 -0500

    Update spaceship.js

--- spaceship.js
+++ spaceship.js
@@ -5,8 +5,8 @@ var Spaceship = function(x, y) {
   this.display = function() {
     fill(223, 224, 182);
     ellipse(this.pos.x, this.pos.y, this.size, this.size);
-    
-    
+
+
     //     Move spaceship
     if (keyIsPressed && keyCode === 38) {
       this.pos.y -= 2;
@@ -24,9 +24,8 @@ var Spaceship = function(x, y) {
       rect(150, 150, 300, 300);
       textSize(18);
       fill(0, 0, 0);
-      text(questions[i], 200, 200);
-      // text(answers1[i], 200, 300);
-      // text(answers2[i], 200, 350);
+      text(questions1[i], 200, 200);
+
 
     }
 
@@ -36,8 +35,19 @@ var Spaceship = function(x, y) {
       rect(150, 150, 300, 300);
       textSize(18);
       fill(0, 0, 0);
-      text(questions[i], 200, 200);
+      text(questions1[i], 200, 200);
 
+    }
+
+    //     Constrain spaceship inside the canvas
+    if (this.pos.y < 0) {
+      this.pos = createVector(this.pos.x, 0);
+    } else if (this.pos.y > 600) {
+      this.pos = createVector(this.pos.x, 600);
+    } else if (this.pos.x < 0) {
+      this.pos = createVector(0, this.pos.y)
+    } else if (this.pos.x > 600) {
+      this.pos = createVector(600, this.pos.y);
     }
 
   }
