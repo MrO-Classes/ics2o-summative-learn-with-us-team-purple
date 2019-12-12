c2bbcc1afe21fb8bba89a1982d0de5b339f72e30
RachaelED <55112454+RachaelED@users.noreply.github.com>
2019-12-12 11:54:04 -0500

    Create file splashScreen.js

---
+++ splashScreen.js
@@ -0,0 +1,18 @@
+var Scene1 = function() {
+  background(0, 0, 0);
+  fill(255, 255, 255);
+  textSize(50);
+  text("NAME", 250, 100);
+  textSize(30);
+  text("About", 250, 250);
+  text("Main Menu", 250, 300);
+  textSize(40);
+  fill(255, 0, 0); 
+  text("PLAY!", 250, 350);
+  
+//   Logo
+  fill(0, 55, 194);
+  ellipse(500, 500, 100, 100);
+
+
+}
