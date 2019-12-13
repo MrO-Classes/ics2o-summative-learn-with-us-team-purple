a5222c15cf5d0d7aba25b5241ce82ed3173d2ce6
RachaelED <55112454+RachaelED@users.noreply.github.com>
2019-12-13 10:24:46 -0500

    Add buttons from splash screen to options

--- sketch.js
+++ sketch.js
@@ -17,18 +17,52 @@ function setup() {
 function draw() {
   background(0, 0, 0);
 
-//   Draw each Scene
+  //   Draw each Scene
   if (sceneNum === 0) {
     Scene1();
   } else if (sceneNum === 1) {
+    About();
+    BackButton();
+  } else if (sceneNum === 2) {
+    MainMenu();
+    BackButton();
+  } else if (sceneNum === 3) {
+    Instructions();
+    BackButton();
+  } else if (sceneNum === 4) {
+    BackButton();
     oxygen.display();
     water.display();
     spaceship.display();
   }
 
-//   Increment sceneNum by 1 with mouseClicked
-  mouseClicked = function() {
+}
 
-    sceneNum++;
+
+
+//   Increment sceneNum by 1 with mouseClicked
+mouseClicked = function() {
+  //     About Screen
+  if (mouseX <= 350 && mouseX >= 240 && mouseY <= 250 && mouseY >= 200 && sceneNum === 0) {
+    sceneNum = 1;
   }
+
+  //     Main Menu Screen
+  if (mouseX <= 400 && mouseX >= 250 && mouseY <= 300 && mouseY >= 275 && sceneNum === 0) {
+    sceneNum = 2;
+  }
+  //     Instructions
+  if (mouseX <= 400 && mouseX >= 250 && mouseY <= 375 && mouseY >= 300 && sceneNum === 0) {
+    sceneNum = 3;
+  }
+  
+  if (mouseX <= 400 && mouseX >=250 && mouseY <=400 && mouseY >=350) {
+  sceneNum = 4;
+  }
+
+  //       Use "Back Button" to return to splsah screen
+  if (mouseX <= 110 && mouseX >= 10 && mouseY <= 60 && mouseY >= 10) {
+    sceneNum = 0;
+  }
+
 }
