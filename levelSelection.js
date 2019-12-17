9ccd2f247a59e7b21aa732a48596565d1a536464
RachaelED <55112454+RachaelED@users.noreply.github.com>
2019-12-17 10:26:53 -0500

    Create file to display different level options

---
+++ levelSelection.js
@@ -0,0 +1,11 @@
+var LevelSelection = function() {
+  BackButton();
+  fill(255, 255, 255);
+//   Level 1
+  rect(60, 200, 120, 120);
+//   Level 2
+  rect(240, 200, 120, 120);
+//   Level 3
+  rect(420, 200, 120, 120);
+
+}
