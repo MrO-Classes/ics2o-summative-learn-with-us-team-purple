8c529a3a670b430a40d348ca159a7bbae0cb3f3c
RachaelED <55112454+RachaelED@users.noreply.github.com>
2019-12-18 11:53:44 -0500

    Add labels in level selection

--- levelSelection.js
+++ levelSelection.js
@@ -1,11 +1,20 @@
 var LevelSelection = function() {
   BackButton();
   fill(255, 255, 255);
+  textSize(20);
 //   Level 1
   rect(60, 200, 120, 120);
+  fill(0, 0, 0);
+  text("Level 1", 85, 250);
 //   Level 2
+  fill(255, 255,255);
   rect(240, 200, 120, 120);
+  fill(0, 0, 0);
+  text("Level 2", 265, 250);
 //   Level 3
+  fill(255, 255, 255);
   rect(420, 200, 120, 120);
+  fill(0, 0, 0);
+  text("Level 3", 445, 250);
 
 }
