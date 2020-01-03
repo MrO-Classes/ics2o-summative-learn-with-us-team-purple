96e62cfcd3befe6920e0dc6610c41c07b1c87b96
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-02 23:31:57 -0500

    Add constraints
    Constrain when asteroids can or cannot touch spaceship

--- level2.js
+++ level2.js
@@ -1,34 +1,17 @@
 var Level2 = function() {
-  ASTEROID.display();
-  ASTEROID2.display();
-  ASTEROID3.display();
-  
-//   Check if SPACESHIP is touching asteroids
-  if (ASTEROID.pos.y <= SPACESHIP.pos.y + 20 && ASTEROID.pos.y >= SPACESHIP.pos.y - 20 && ASTEROID.pos.x <= SPACESHIP.pos.x + 20 && ASTEROID.pos.x >= SPACESHIP.pos.x - 20) {
+
+//   Check if asteroids are touching spaceship, and fail level if they are, but if spacehsip is touching oxygen or water, then don't fail level
+  if (!(SPACESHIP.pos.x <= OXYGEN.pos.x + 22.5 && SPACESHIP.pos.x >= OXYGEN.pos.x - 22.5 && SPACESHIP.pos.y <= OXYGEN.pos.y + 22.5 && SPACESHIP.pos.y >= OXYGEN.pos.y - 22.5) && ! (SPACESHIP.pos.x <= WATER.pos.x + 15 && SPACESHIP.pos.x >= WATER.pos.x - 15 && SPACESHIP.pos.y <= WATER.pos.y + 15 && SPACESHIP.pos.y >= WATER.pos.y - 15) && ASTEROID1.pos.x <= SPACESHIP.pos.x + 20 && ASTEROID1.pos.x >= SPACESHIP.pos.x - 20 && ASTEROID1.pos.y <= SPACESHIP.pos.y + 20 && ASTEROID1.pos.y >= SPACESHIP.pos.y - 20) {
   sceneNum = 8;
-  } else if (ASTEROID2.pos.y <= SPACESHIP.pos.y + 20 && ASTEROID2.pos.y >= SPACESHIP.pos.y - 20 && ASTEROID2.pos.x <= SPACESHIP.pos.x + 20 && ASTEROID2.pos.x >= SPACESHIP.pos.x - 20) {
+} else if (!(SPACESHIP.pos.x <= OXYGEN.pos.x + 22.5 && SPACESHIP.pos.x >= OXYGEN.pos.x - 22.5 && SPACESHIP.pos.y <= OXYGEN.pos.y + 22.5 && SPACESHIP.pos.y >= OXYGEN.pos.y - 22.5) && !(SPACESHIP.pos.x <= WATER.pos.x + 15 && SPACESHIP.pos.x >= WATER.pos.x - 15 && SPACESHIP.pos.y <= WATER.pos.y + 15 && SPACESHIP.pos.y >= WATER.pos.y - 15) && ASTEROID2.pos.x <= SPACESHIP.pos.x + 20 && ASTEROID2.pos.x >= SPACESHIP.pos.x - 20 && ASTEROID2.pos.y <= SPACESHIP.pos.y + 20 && ASTEROID2.pos.y >= SPACESHIP.pos.y - 20) {
   sceneNum = 8;
-  } else if (ASTEROID3.pos.y <= SPACESHIP.pos.y + 20 && ASTEROID3.pos.y >= SPACESHIP.pos.y - 20 && ASTEROID3.pos.x <= SPACESHIP.pos.x + 20 && ASTEROID3.pos.x >= SPACESHIP.pos.x - 20) {
+} else if (!(SPACESHIP.pos.x <= OXYGEN.pos.x + 22.5 && SPACESHIP.pos.x >= OXYGEN.pos.x - 22.5 && SPACESHIP.pos.y <= OXYGEN.pos.y + 22.5 && SPACESHIP.pos.y >= OXYGEN.pos.y - 22.5) && !(SPACESHIP.pos.x <= WATER.pos.x + 15 && SPACESHIP.pos.x >= WATER.pos.x - 15 && SPACESHIP.pos.y <= WATER.pos.y + 15 && SPACESHIP.pos.y >= WATER.pos.y - 15) &&  ASTEROID3.pos.x <= SPACESHIP.pos.x + 20 && ASTEROID3.pos.x >= SPACESHIP.pos.x - 20 && ASTEROID3.pos.y <= SPACESHIP.pos.y + 20 && ASTEROID3.pos.y >= SPACESHIP.pos.y - 20) {
   sceneNum = 8;
-  }
-  
-  
-//   Check if SPACESHIP is touching OXYGEN
-  if (SPACESHIP.pos.x <= OXYGEN.pos.x + 22.5 && SPACESHIP.pos.x >= OXYGEN.pos.x - 22.5 && SPACESHIP.pos.y <= OXYGEN.pos.y + 22.5 && SPACESHIP.pos.y >= OXYGEN.pos.y - 22.5) {
-      fill(255, 255, 255);
-      rect(150, 150, 300, 300);
-      textSize(18);
-      fill(0, 0, 0);
-      text(questions1[i], 200, 200);
-      text(answers1[i], 200, 250);
-      text(answers2[i], 200, 300);
-      text(answers3[i], 200, 350);
-      text(answers4[i], 200, 400);   
-      fill(0, 255, 0);
-      rect(365, 390, 75, 50);
-      fill(0, 0, 0);
-      text("Check", 375, 420);
-    }
+} else if (!(SPACESHIP.pos.x <= OXYGEN.pos.x + 22.5 && SPACESHIP.pos.x >= OXYGEN.pos.x - 22.5 && SPACESHIP.pos.y <= OXYGEN.pos.y + 22.5 && SPACESHIP.pos.y >= OXYGEN.pos.y - 22.5) && !(SPACESHIP.pos.x <= WATER.pos.x + 15 && SPACESHIP.pos.x >= WATER.pos.x - 15 && SPACESHIP.pos.y <= WATER.pos.y + 15 && SPACESHIP.pos.y >= WATER.pos.y - 15) &&  ASTEROID4.pos.x <= SPACESHIP.pos.x + 20 && ASTEROID4.pos.x >= SPACESHIP.pos.x - 20 && ASTEROID4.pos.y <= SPACESHIP.pos.y + 20 && ASTEROID4.pos.y >= SPACESHIP.pos.y - 20) {
+  sceneNum = 8;
+} else if (!(SPACESHIP.pos.x <= OXYGEN.pos.x + 22.5 && SPACESHIP.pos.x >= OXYGEN.pos.x - 22.5 && SPACESHIP.pos.y <= OXYGEN.pos.y + 22.5 && SPACESHIP.pos.y >= OXYGEN.pos.y - 22.5) && !(SPACESHIP.pos.x <= WATER.pos.x + 15 && SPACESHIP.pos.x >= WATER.pos.x - 15 && SPACESHIP.pos.y <= WATER.pos.y + 15 && SPACESHIP.pos.y >= WATER.pos.y - 15) &&  ASTEROID5.pos.x <= SPACESHIP.pos.x + 20 && ASTEROID5.pos.x >= SPACESHIP.pos.x - 20 && ASTEROID5.pos.y <= SPACESHIP.pos.y + 20 && ASTEROID5.pos.y >= SPACESHIP.pos.y - 20) {
+  sceneNum = 8;
+}
 
   fill(0, 255, 0);
   rect(500, 500, 100, 100);
@@ -37,13 +20,55 @@ var Level2 = function() {
   text("Level 2", 10, 525);
   text("Score: " + SPACESHIP.score, 10, 550);
 
-//   Display Objects
+  //   Display Objects
+  ASTEROID1.display();
+  ASTEROID2.display();
+  ASTEROID3.display();
+  ASTEROID4.display();
+  ASTEROID5.display();
   BackButton();
   OXYGEN.display();
   WATER.display();
   SPACESHIP.display();
+  
+  //   Check if SPACESHIP is touching OXYGEN
+  if (SPACESHIP.pos.x <= OXYGEN.pos.x + 22.5 && SPACESHIP.pos.x >= OXYGEN.pos.x - 22.5 && SPACESHIP.pos.y <= OXYGEN.pos.y + 22.5 && SPACESHIP.pos.y >= OXYGEN.pos.y - 22.5) {
+    fill(255, 255, 255);
+    rect(150, 150, 300, 300);
+    textSize(18);
+    fill(0, 0, 0);
+    text(questions1[i], 200, 200);
+    text(answers1[i], 200, 250);
+    text(answers2[i], 200, 300);
+    text(answers3[i], 200, 350);
+    text(answers4[i], 200, 400);
+    fill(0, 255, 0);
+    rect(365, 390, 75, 50);
+    fill(0, 0, 0);
+    text("Check", 375, 420);
 
-//   Check if SPACESHIP is on rectangle (win the level)
+  }
+  
+      //     Check if SPACESHIP is touching water
+    if (SPACESHIP.pos.x <= WATER.pos.x + 15 && SPACESHIP.pos.x >= WATER.pos.x - 15 && SPACESHIP.pos.y <= WATER.pos.y + 15 && SPACESHIP.pos.y >= WATER.pos.y - 15) {
+      fill(255, 255, 255);
+      rect(150, 150, 300, 300);
+      textSize(18);
+      fill(0, 0, 0);
+      text(questions1[i], 200, 200);
+      text(answers1[i], 200, 250);
+      text(answers2[i], 200, 300);
+      text(answers3[i], 200, 350);
+      text(answers4[i], 200, 400);
+      fill(0, 255, 0);
+      rect(365, 390, 75, 50);
+      fill(0, 0, 0);
+      text("Check", 375, 420);
+
+    }
+
+
+  //   Check if SPACESHIP is on rectangle (win the level)
   if (SPACESHIP.pos.x <= 600 && SPACESHIP.pos.x >= 500 && SPACESHIP.pos.y <= 600 && SPACESHIP.pos.y >= 500) {
     sceneNum = 7;
   }
@@ -72,7 +97,7 @@ var WinState = function() {
   fill(0, 0, 0);
   text("Next", 330, 475);
   text("Level", 330, 495);
-  
+
 }
 
 // Display if Level Lost (Hit by Asteroid)
@@ -94,6 +119,6 @@ var LoseState = function() {
   fill(0, 0, 0);
   text("Retry", 330, 475);
   text("Level", 330, 495);
-  
-  
+
+
 }
