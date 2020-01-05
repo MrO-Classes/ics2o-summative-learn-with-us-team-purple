94a291e96f167bc58b93949ca32fd482c161f03e
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-05 16:53:20 -0500

    Update water.js

--- water.js
+++ water.js
@@ -1,10 +1,10 @@
-var Water = function (x, y) {
-this.pos = createVector (400, 200);
+var Water = function(x, y) {
+  this.pos = createVector(x, y);
   this.size = 35;
-  
+
   this.display = function() {
     fill(45, 137, 207);
-  ellipse(this.pos.x, this.pos.y, this.size, this.size);
+    ellipse(this.pos.x, this.pos.y, this.size, this.size);
   }
-  
+
 }
