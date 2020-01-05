d2117a76dd6e64e7cff1bfece34bab83e4046195
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-05 16:54:16 -0500

    Add functions for right/wrong answers to questions

--- level2.js
+++ level2.js
@@ -1,5 +1,6 @@
 var Level2 = function() {
 
+  
 //   Check if asteroids are touching spaceship, and fail level if they are, but if spacehsip is touching oxygen or water, then don't fail level
   if (!(SPACESHIP.pos.x <= OXYGEN.pos.x + 22.5 && SPACESHIP.pos.x >= OXYGEN.pos.x - 22.5 && SPACESHIP.pos.y <= OXYGEN.pos.y + 22.5 && SPACESHIP.pos.y >= OXYGEN.pos.y - 22.5) && ! (SPACESHIP.pos.x <= WATER.pos.x + 15 && SPACESHIP.pos.x >= WATER.pos.x - 15 && SPACESHIP.pos.y <= WATER.pos.y + 15 && SPACESHIP.pos.y >= WATER.pos.y - 15) && ASTEROID1.pos.x <= SPACESHIP.pos.x + 20 && ASTEROID1.pos.x >= SPACESHIP.pos.x - 20 && ASTEROID1.pos.y <= SPACESHIP.pos.y + 20 && ASTEROID1.pos.y >= SPACESHIP.pos.y - 20) {
   sceneNum = 8;
@@ -12,7 +13,16 @@ var Level2 = function() {
 } else if (!(SPACESHIP.pos.x <= OXYGEN.pos.x + 22.5 && SPACESHIP.pos.x >= OXYGEN.pos.x - 22.5 && SPACESHIP.pos.y <= OXYGEN.pos.y + 22.5 && SPACESHIP.pos.y >= OXYGEN.pos.y - 22.5) && !(SPACESHIP.pos.x <= WATER.pos.x + 15 && SPACESHIP.pos.x >= WATER.pos.x - 15 && SPACESHIP.pos.y <= WATER.pos.y + 15 && SPACESHIP.pos.y >= WATER.pos.y - 15) &&  ASTEROID5.pos.x <= SPACESHIP.pos.x + 20 && ASTEROID5.pos.x >= SPACESHIP.pos.x - 20 && ASTEROID5.pos.y <= SPACESHIP.pos.y + 20 && ASTEROID5.pos.y >= SPACESHIP.pos.y - 20) {
   sceneNum = 8;
 }
+  
+  
+  
+  if (SHOOTER.pos.x <= ASTEROID1.pos.x + 20 && SHOOTER.pos.x >= ASTEROID1.pos.x - 20 && SHOOTER.pos.y <= ASTEROID1.pos.y + 20 && SHOOTER.pos.x >= ASTEROID1.pos.y - 20) {
+    rect(10, 10, 10, 10);
+  }
+  
+ 
 
+//   Display in bottom, left corner of game
   fill(0, 255, 0);
   rect(500, 500, 100, 100);
   fill(255, 255, 255);
@@ -30,40 +40,44 @@ var Level2 = function() {
   OXYGEN.display();
   WATER.display();
   SPACESHIP.display();
+  SHOOTER.display();
+  
   
   //   Check if SPACESHIP is touching OXYGEN
   if (SPACESHIP.pos.x <= OXYGEN.pos.x + 22.5 && SPACESHIP.pos.x >= OXYGEN.pos.x - 22.5 && SPACESHIP.pos.y <= OXYGEN.pos.y + 22.5 && SPACESHIP.pos.y >= OXYGEN.pos.y - 22.5) {
     fill(255, 255, 255);
-    rect(150, 150, 300, 300);
+    rect(50, 100, 500, 400);
     textSize(18);
     fill(0, 0, 0);
-    text(questions1[i], 200, 200);
-    text(answers1[i], 200, 250);
-    text(answers2[i], 200, 300);
-    text(answers3[i], 200, 350);
-    text(answers4[i], 200, 400);
+    text(questionsO1[i], 70, 150);
+    text(questionsO2[i], 70, 180); 
+    text(answersO1[i], 70, 250);
+    text(answersO2[i], 70, 300);
+    text(answersO3[i], 70, 350);
+    text(answersO4[i], 70, 400);
     fill(0, 255, 0);
-    rect(365, 390, 75, 50);
-    fill(0, 0, 0);
-    text("Check", 375, 420);
+    // rect(365, 390, 75, 50);
+    // fill(0, 0, 0);
+    // text("Check", 375, 420);
 
   }
   
       //     Check if SPACESHIP is touching water
     if (SPACESHIP.pos.x <= WATER.pos.x + 15 && SPACESHIP.pos.x >= WATER.pos.x - 15 && SPACESHIP.pos.y <= WATER.pos.y + 15 && SPACESHIP.pos.y >= WATER.pos.y - 15) {
       fill(255, 255, 255);
-      rect(150, 150, 300, 300);
+      rect(50, 100, 500, 400);
       textSize(18);
       fill(0, 0, 0);
-      text(questions1[i], 200, 200);
-      text(answers1[i], 200, 250);
-      text(answers2[i], 200, 300);
-      text(answers3[i], 200, 350);
-      text(answers4[i], 200, 400);
+      text(questionsW1[w], 70, 150);
+      text(questionsW2[w], 70, 180);
+      text(answersW1[w], 70, 250);
+      text(answersW2[w], 70, 300);
+      text(answersW3[w], 70, 350);
+      text(answersW4[w], 70, 400);
       fill(0, 255, 0);
-      rect(365, 390, 75, 50);
-      fill(0, 0, 0);
-      text("Check", 375, 420);
+      // rect(365, 390, 75, 50);
+      // fill(0, 0, 0);
+      // text("Check", 375, 420);
 
     }
 
@@ -97,6 +111,7 @@ var WinState = function() {
   fill(0, 0, 0);
   text("Next", 330, 475);
   text("Level", 330, 495);
+  SPACESHIP.pos = createVector(100, 100);
 
 }
 
@@ -120,5 +135,35 @@ var LoseState = function() {
   text("Retry", 330, 475);
   text("Level", 330, 495);
 
+}
 
+// Correct Oxygen question answer
+var CorrectAnswerOxygen = function() {
+  SPACESHIP.score +=10;
+  i = 5;
+  
+}
+
+// Wrong Oxygen question answer
+var WrongAnswerOxygen = function() {
+  i ++;
+  
+  if (i === 5) {
+    i = 0;
+  }
+}
+
+// Correct Water question answer
+var CorrectAnswerWater = function() {
+  SPACESHIP.score +=10;
+  w = 4;
+}
+
+// Wrong Water question answer
+var WrongAnswerWater = function() {
+  w ++;
+  
+  if (w === 4) {
+    w = 0;
+  }
 }
