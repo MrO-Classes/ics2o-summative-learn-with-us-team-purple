233777ebbc51448a0a708cac5c24d0e0aa6891e5
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-08 11:46:52 -0500

    Add Correct and WrongAnswer functions
    Remove Correct and WrongAnswer functions from level1.js and level2.js and condense inside water.js

--- water.js
+++ water.js
@@ -6,5 +6,28 @@ var Water = function(x, y) {
     fill(45, 137, 207);
     ellipse(this.pos.x, this.pos.y, this.size, this.size);
   }
+  
+  this.CorrectAnswer = function() {
+    if (sceneNum === 5) {
+      spaceship.score +=10;
+      w = 4;
+    } else if (sceneNum === 6) {
+      SPACESHIP.score +=10;
+      W = 4;
+    }
+    
+    
+  }
+  
+  this.WrongAnswer = function() {
+    w ++;
+    W ++;
+    
+    if (w === 4) {
+      w = 0;
+    } else if (W === 4) {
+      W = 0;
+    }
+  }
 
 }
