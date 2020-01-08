832031a3241b8f26a7d6d43a93cf9284bcc983a0
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-08 11:45:21 -0500

    Add correct and wrong answer functions
    Remove correct and wrong answers from level1.js and level2.js and condense code to oxygen.js

--- oxygen.js
+++ oxygen.js
@@ -7,4 +7,25 @@ var Oxygen = function(x, y) {
     ellipse(this.pos.x, this.pos.y, this.size, this.size);
 
   }
+  
+  this.CorrectAnswer = function() {
+    if (sceneNum === 5) {
+      spaceship.score +=10;
+      o = 4;
+    } else if (sceneNum === 6) {
+      SPACESHIP.score +=10;
+      O = 5;
+    }
+  }
+  
+  this.WrongAnswer = function() {
+    O ++;
+    o ++;
+    
+    if (o === 4) {
+      o = 0;
+    } else if (O === 5) {
+      O = 0;
+    }
+  }
 }
