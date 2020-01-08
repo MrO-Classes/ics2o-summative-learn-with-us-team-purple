d72f00161218687ec29fdfaf99ce89be25b16b57
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-07 21:24:22 -0500

    Update shooter.js

--- shooter.js
+++ shooter.js
@@ -3,10 +3,11 @@ var Shooter = function(x, y) {
   this.size = 10;
   
   this.display = function(x, y) {
+    ellipse(this.pos.x, this.pos.y, this.size, this.size);
     
-     if (keyCode === 32) {
-       ellipse(this.pos.x, this.pos.y, this.size, this.size);
-     }
+    if (keyCode === 32) {
+      this.pos.x +=2;
+    }
     
 //     if (keyIsPressed && keyCode === 32) {
 //     fill(255, 0, 0);
@@ -17,4 +18,4 @@ var Shooter = function(x, y) {
 //     }
   }
 
-}
+} 
