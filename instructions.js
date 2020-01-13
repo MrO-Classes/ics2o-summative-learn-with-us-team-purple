b431e362961b8d35ab01cad4898f602968d92b4f
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-13 10:29:16 -0500

    Add info for instructions.js

--- instructions.js
+++ instructions.js
@@ -1,6 +1,19 @@
 var Instructions = function() {
+  textSize(50);
   background(0, 0, 0);
   fill(255, 255, 255);
-  text("Instructions", 100, 100);
+  text("Instructions", 175, 100);
+  
+  textSize(20);
+  text("The Earth is losing all of its life! You have been chosen ", 50, 190);
+  text("to complete a dangerous mission to save the planet!", 50, 220);
+  
+  text("- use the arow keys to control the spaceship", 50, 260);
+  text("- land on oxygen and water elements (white and blue ", 50, 290);
+  text("circles) and complete the questions to survive", 60, 320);
+  text("- don't get hit by the asteroids (or you restart the level)", 50, 350);
+  text("- to complete the level, land on the green square", 50, 380);
+  text("- work your way towards collecting the ring (last level)", 50, 410);
+  text("and bring it back home to save the planet", 60, 440);
 
 }
