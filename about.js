23da093eb308dc4bde368417a3adf2dc553023ee
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-13 10:28:30 -0500

    Add info to about.js

--- about.js
+++ about.js
@@ -1,8 +1,20 @@
 var About = function() {
+  textSize(50);
   background(0, 0, 0);
   textSize(50);
   fill(255, 255, 255);
-  text("About Us", 150, 150);
+  text("About Us", 190, 100);
   
+  textSize(20);
+  text("Rachael Driediger and Afnan Kabir are grade 9 students ", 50, 190);
+  text("at A.Y. Jackson SS. They are proud to present this game ", 50, 220);
+  text("inspired by two third grade students at St. Martin de Porres", 50, 250);
+  text("school, Maryam and David's love for space. Rachael", 50, 280);
+  text("and Afnan enjoyed coding this game, and incorporating", 50, 310);
+  text("artwork drawn by Maryam and David. They both learned", 50, 340);
+  text("much about coding, and worked hard to produce the final", 50, 370);
+  text("product. They hope that you will enjoy playing what they", 50, 400);
+  text("have created, and they hope this will inspire you to learn to", 50, 430);
+  text("create using code!", 50, 460);
 
 }
