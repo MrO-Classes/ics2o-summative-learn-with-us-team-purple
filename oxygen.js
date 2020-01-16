3f016e2e68842f5b3547a0c13a37d10d1fd475d2
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-16 11:41:21 -0500

    Add sceneNum constraints

--- oxygen.js
+++ oxygen.js
@@ -9,23 +9,31 @@ var Oxygen = function(x, y) {
   }
   
   this.CorrectAnswer = function() {
+//     Display if oxygen question is answerd correctly
     if (sceneNum === 5) {
       spaceship.score +=10;
       o = 4;
     } else if (sceneNum === 6) {
       SPACESHIP.score +=10;
       O = 5;
+    } else if (sceneNum === 9) {
+      spaceship3.score +=10;
+      o3 = 6;
     }
   }
   
   this.WrongAnswer = function() {
+//     Display if oxygen question is answered incorrectly
     O ++;
     o ++;
+    o3++;
     
     if (o === 4) {
       o = 0;
     } else if (O === 5) {
       O = 0;
+    } else if (o3 === 6) {
+      o3 = 0;
     }
   }
 }
