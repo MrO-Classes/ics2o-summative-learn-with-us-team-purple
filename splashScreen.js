a87740f138b5e8867ab7838901e9e9a89688556f
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-16 11:46:49 -0500

    Create a splash screen
    Create an animated splash screen. This can be modified and improved.

--- splashScreen.js
+++ splashScreen.js
@@ -1,3 +1,23 @@
 var SplashScreen = function() {
-  image(img, 200, 400, 200, 100);
+  var imgX = 0;
+  var imgY = 200;
+  var imgW = 200;
+  var imgH = 100;
+
+  this.display = function() {
+    image(img, imgX, imgY, imgW, imgH);
+    
+    if (imgX < 200) {
+      imgX +=2;
+    } else if (imgX === 200) {
+      imgX = 200;
+      
+      fill(255, 255, 255);
+      rect(200, 400, 200, 100);
+      fill(0, 0, 0);
+      textSize(20);
+      text("Click here to play", 220, 450);
+    }
+  }
+
 }
