0d61600c78b97acd79797ceddde7e1d1123acba1
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-16 11:41:51 -0500

    Add sceneNum constraints

--- water.js
+++ water.js
@@ -8,25 +8,38 @@ var Water = function(x, y) {
   }
   
   this.CorrectAnswer = function() {
+//     Display if water question is answered correctly
     if (sceneNum === 5) {
       spaceship.score +=10;
       w = 4;
     } else if (sceneNum === 6) {
       SPACESHIP.score +=10;
       W = 4;
+    } else if (sceneNum === 9) {
+      spaceship3.score +=10;
+      w3 = 6;
     }
     
     
   }
   
   this.WrongAnswer = function() {
-    w ++;
-    W ++;
+//     Display if water question is answered incorrectly
+    
+    if (sceneNum === 5) {
+      w ++;
+    } else if (sceneNum === 6) {
+      W ++;
+    } else if (sceneNum === 0) {
+      w3 ++;
+    }
     
     if (w === 4) {
       w = 0;
     } else if (W === 4) {
       W = 0;
+    } else if (w3 === 6) {
+      w3 = 0;
     }
   }
 
