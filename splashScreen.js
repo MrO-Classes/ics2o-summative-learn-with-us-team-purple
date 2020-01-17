ee77f079bc37692f72023e5ce941d7a0cf077b5e
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-17 10:31:39 -0500

    Add star images to background

--- splashScreen.js
+++ splashScreen.js
@@ -5,6 +5,13 @@ var SplashScreen = function() {
   var imgH = 100;
 
   this.display = function() {
+    image(img1, 100, 100, 40, 40);
+  image(img1, 75, 300, 40, 40);
+  image(img1, 200, 500, 40, 40);
+  image(img1, 400, 250, 40, 40);
+  image(img1, 500, 400, 40, 40);
+  image(img1, 550, 50, 40, 40);
+
     image(img, imgX, imgY, imgW, imgH);
     
     if (imgX < 200) {
