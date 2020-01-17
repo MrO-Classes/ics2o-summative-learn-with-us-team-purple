4cf210a16499a82627ab60b09521c7c3b9feffa0
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-17 10:28:51 -0500

    Add star images to background

--- mainMenu.js
+++ mainMenu.js
@@ -1,5 +1,12 @@
 var MainMenu = function() {
   background(0, 0, 0);
+  image(img1, 100, 100, 40, 40);
+  image(img1, 75, 300, 40, 40);
+  image(img1, 200, 500, 40, 40);
+  image(img1, 400, 250, 40, 40);
+  image(img1, 500, 400, 40, 40);
+  image(img1, 550, 50, 40, 40);
+
   fill(255, 255, 255);
   textSize(50);
   text("Main Menu", 175, 100);
