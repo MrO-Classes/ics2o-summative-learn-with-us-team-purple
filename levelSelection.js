c188972b207c03223fecbb6cbbc5bc88dc8c2183
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-17 10:29:14 -0500

    Add star images to background

--- levelSelection.js
+++ levelSelection.js
@@ -1,5 +1,12 @@
 var LevelSelection = function() {
   BackButton();
+  image(img1, 100, 100, 40, 40);
+  image(img1, 75, 300, 40, 40);
+  image(img1, 200, 500, 40, 40);
+  image(img1, 400, 250, 40, 40);
+  image(img1, 500, 400, 40, 40);
+  image(img1, 550, 50, 40, 40);
+
   fill(255, 255, 255);
   textSize(20);
 //   Level 1
