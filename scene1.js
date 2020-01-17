7e553706772886e514a6e12ac875c934dcfb5cee
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-17 10:27:40 -0500

    Add star images to background

--- scene1.js
+++ scene1.js
@@ -1,7 +1,16 @@
 var Scene1 = function() {
-  
-  
+
+
   background(0, 0, 0);
+
+  image(img1, 100, 100, 40, 40);
+  image(img1, 75, 300, 40, 40);
+  image(img1, 200, 500, 40, 40);
+  image(img1, 400, 250, 40, 40);
+  image(img1, 500, 400, 40, 40);
+  image(img1, 550, 50, 40, 40);
+
+
   fill(250, 241, 65);
   textSize(50);
   text("STARS", 235, 100);
@@ -12,18 +21,17 @@ var Scene1 = function() {
   textSize(30);
   fill(255, 255, 255);
   text("About", 250, 250);
-  text("Main Menu", 250, 300); 
+  text("Main Menu", 250, 300);
   text("Instructions", 250, 350);
   fill(255, 0, 0);
   textSize(40);
   text("PLAY!", 250, 400);
-  
-//   Logo
+
+  //   Logo
   fill(0, 55, 194);
-  // ellipse(500, 500, 100, 100);
   image(img, 390, 500, 200, 50);
 
-  
+
 
 
 }
