03e982db782bc0787562a252b971e992669c593c
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-19 18:10:57 -0500

    Fix typos

--- instructions.js
+++ instructions.js
@@ -15,9 +15,9 @@ var Instructions = function() {
   text("The Earth is losing all of its life! You have been chosen ", 50, 190);
   text("to complete a dangerous mission to save the planet!", 50, 220);
   
-  text("- use the arow keys to control the spaceship", 50, 260);
-  text("- land on oxygen and water elements (white and blue ", 50, 290);
-  text("circles) and complete the questions to survive", 60, 320);
+  text("- use the arrow keys to control the spaceship", 50, 260);
+  text("- land on oxygen and water elements and ", 50, 290);
+  text("complete the questions to survive", 60, 320);
   text("- don't get hit by the asteroids (or you restart the level)", 50, 350);
   text("- to complete the level, land on the green square", 50, 380);
   text("- work your way towards collecting the ring (last level)", 50, 410);
