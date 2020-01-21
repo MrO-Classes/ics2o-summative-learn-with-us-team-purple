8e3ded3c00bfc9bc3799295c683d8b4469079938
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-21 10:26:18 -0500

    Update splashScreen.js

--- splashScreen.js
+++ splashScreen.js
@@ -6,19 +6,19 @@ var SplashScreen = function() {
 
   this.display = function() {
     image(img1, 100, 100, 40, 40);
-  image(img1, 75, 300, 40, 40);
-  image(img1, 200, 500, 40, 40);
-  image(img1, 400, 250, 40, 40);
-  image(img1, 500, 400, 40, 40);
-  image(img1, 550, 50, 40, 40);
+    image(img1, 75, 300, 40, 40);
+    image(img1, 200, 500, 40, 40);
+    image(img1, 400, 250, 40, 40);
+    image(img1, 500, 400, 40, 40);
+    image(img1, 550, 50, 40, 40);
 
     image(img, imgX, imgY, imgW, imgH);
-    
+
     if (imgX < 200) {
-      imgX +=2;
+      imgX += 2;
     } else if (imgX === 200) {
       imgX = 200;
-      
+
       fill(255, 255, 255);
       rect(200, 400, 200, 100);
       fill(0, 0, 0);
