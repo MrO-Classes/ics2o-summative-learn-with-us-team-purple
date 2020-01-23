58c31adeddaaf62317de248ab9f4cfc910c999c1
RachaelED <55112454+RachaelED@users.noreply.github.com>
2020-01-23 09:29:50 -0500

    Complete levelSelection.js

--- levelSelection.js
+++ levelSelection.js
@@ -1,5 +1,6 @@
 var LevelSelection = function() {
   BackButton();
+//   Star images
   image(img1, 100, 100, 40, 40);
   image(img1, 75, 300, 40, 40);
   image(img1, 200, 500, 40, 40);
@@ -25,4 +26,3 @@ var LevelSelection = function() {
   text("Level 3", 445, 250);
 
 }
-
